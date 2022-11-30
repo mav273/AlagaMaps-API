df913d8e3e7ba0417d12bfd44982075d349505e8
Marcus Vinicius <marcusvslourenco@fac.pe.senac.br>
2022-11-29 23:00:50 -0300

    funções usadas anteriormente removidas

--- sequelize/controllers/coordinates_controllers.js
+++ sequelize/controllers/coordinates_controllers.js
@@ -1,55 +1,27 @@
 const res = require('express/lib/response.js');
 const { sequelize, coordinates } = require('../models/index.js');
 
-exports.buscarId = async () => {
+buscarId = async () => {
   const id = await sequelize.query(
     `SELECT id from coordinates order by id desc limit 1`,
   );
   return id[0][0].id;
 };
 
-exports.mostrarTodos = (req, res) => {
-  sequelize
-    .query(
-      "select type,string_to_array(concat(long,',',lat),',') as coordinates from coordinates;",
-    )
-    .then((data) => {
-      res.send(data[0]);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || 'Ocorreu algum erro ao trazer os dados.',
-      });
-    });
-};
-
-exports.criarPonto = async (req, res) => {
-  const id = await buscarId();
-
-  coordinates
-    .create({
-      id: id + 1,
-      type: req.body.type,
-      long: req.body.long,
-      lat: req.body.lat,
-    })
-    .then((result) => res.json(result))
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || 'Ocorreu um erro ao criar o ponto.',
-      });
-    });
-};
-
 exports.mostrarPontos = async () => {
-  const consulta = sequelize.query(
+  const consulta = await sequelize.query(
     "select type,string_to_array(concat(long,',',lat),',') as coordinates from coordinates;",
   );
-  console.log(consulta[0]);
   return consulta[0];
 };
-exports.criarPont = async (id, type, long, lat) => {
-  const resposta = coordinates.create({
+exports.criarPontos = async (type, long, lat) => {
+  var id = await buscarId();
+  if (isNaN(id)) {
+    id = 1;
+  } else {
+    id = id + 1;
+  }
+  const resposta = await coordinates.create({
     id: id,
     type: type,
     long: long,
