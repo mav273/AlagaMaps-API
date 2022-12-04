84b4dc3e6865356a5157b7a435f38d6d3d576036
Marcus Vinicius <marcusvslourenco@fac.pe.senac.br>
2022-12-04 20:29:42 -0300

    Arquivos realocados para /src/server, rotas de cadastro e login de usuários adicionadas

--- src/server/router.js
+++ src/server/router.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: __dirname + '/../.env' });
 require('dotenv').config();
 const express = require('express');
+const Usuario = require('../classes/classes_users.js');
 const Coordenadas = require('../classes/classe_coordenada.js');
 
 const routes = express.Router();
@@ -11,8 +12,11 @@ routes.get('/', (req, res) => {
 });
 
 var coordenada = new Coordenadas();
+var usuario = new Usuario()
 
 routes.get('/api/pontos/todos', coordenada.mostrar);
 routes.post('/api/pontos/criar', coordenada.criar);
+routes.post('/api/usuarios/checar', usuario.login);
+routes.post('/api/usuarios/criar', usuario.cadastro   );
 
 module.exports = { routes };
